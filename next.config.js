103bcdb431674f0aff5a0c4d12571d0a4629e4bc
omar-faruk02 <tuhin.omar99@gmail.com>
2021-10-25 12:15:40 +0600

    make it better

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 // export const reactStrictMode = true;
 // export const 
  module.exports={reactStrictMode : true,
- env : { MONGO_URI: "mongodb+srv://note-app:note1234@cluster0.gqnwo.mongodb.net/NoteApp?retryWrites=true&w=majority" }}
+ env : { MONGO_URI: "mongodb+srv://note-app:note1234@cluster0.gqnwo.mongodb.net/NoteApp?retryWrites=true&w=majority" },
+BASE_URL:"https://nextnoteapp.herokuapp.com"
+}
