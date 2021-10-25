1ad1113615ceeac921bb1fa9f25fdfb3418af154
omar-faruk02 <tuhin.omar99@gmail.com>
2021-10-25 12:38:55 +0600

    commit

--- next.config.js
+++ next.config.js
@@ -3,4 +3,4 @@
  module.exports={reactStrictMode : true,
  env : { MONGO_URI: "mongodb+srv://note-app:note1234@cluster0.gqnwo.mongodb.net/NoteApp?retryWrites=true&w=majority" },
 BASE_URL:"https://nextnoteapp.herokuapp.com"
-}
+ }
